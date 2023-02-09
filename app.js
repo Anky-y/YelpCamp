75ad4736f0f890b07485f60c0d7834dbeb8dbc1b
Anirudhwa Ankon <ankon495@gmail.com>
2023-02-09 11:22:01 +0600

    Before Adding Reviews

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require(`express`);
+const path = require(`path`);
+const mongoose = require(`mongoose`);
+const Campground = require(`./models/campground`);
+const methodOverride = require(`method-override`);
+const catchAsync = require(`./utils/catchAsync`);
+const ExpressError = require(`./utils/ExpressError`);
+const { campgroundSchema } = require(`./schemas`);
+const ejsMate = require(`ejs-mate`);
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelp-camp", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`MONGO CONNECTION OPEN!!`);
+  })
+  .catch((err) => {
+    console.log(`ERROR MONGO CONNECTION`);
+    console.log(err);
+  });
+
+const app = express();
+app.engine(`ejs`, ejsMate);
+app.set(`view engine`, `ejs`);
+app.set(`views`, path.join(__dirname, `views`));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride(`_method`));
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(`,`);
+    throw new ExpressError(msg, 400);
+  } else next();
+};
+
+app.get(`/`, (req, res) => {
+  res.render(`home`);
+});
+
+app.get(
+  `/campgrounds`,
+  catchAsync(async (req, res) => {
+    const camps = await Campground.find();
+    res.render(`campgrounds/index`, { camps });
+  })
+);
+
+app.get(`/campgrounds/new`, (req, res) => {
+  res.render(`campgrounds/new`);
+});
+
+app.post(
+  `/campgrounds`,
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError(`Invalid Campground data`, 400);
+    const { campground } = req.body;
+    const camp = new Campground(campground);
+    await camp.save();
+    res.redirect(`campgrounds/${camp._id}`);
+  })
+);
+
+app.get(
+  `/campgrounds/:id`,
+  catchAsync(async (req, res) => {
+    const camp = await Campground.findById(req.params.id);
+    res.render(`campgrounds/details`, { camp });
+  })
+);
+
+app.get(
+  `/campgrounds/:id/edit`,
+  catchAsync(async (req, res) => {
+    const camp = await Campground.findById(req.params.id);
+    res.render(`campgrounds/edit`, { camp });
+  })
+);
+
+app.put(
+  `/campgrounds/:id/edit`,
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findOneAndUpdate(id, req.body.campground, { runValidators: true });
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
+app.delete(
+  "/campgrounds/:id/delete",
+  catchAsync(async (req, res) => {
+    await Campground.findByIdAndDelete(req.params.id);
+    res.redirect(`/campgrounds`);
+  })
+);
+
+app.all(`*`, (req, res, next) => {
+  next(new ExpressError(`Page Not found`, 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = `Something Went Wrong:(`;
+  res.status(statusCode).render(`error`, { err });
+});
+
+app.listen(3000, () => {
+  console.log(`On port 3000`);
+});
---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+const mongoose = require(`mongoose`);
+const Campground = require(`../models/campground`);
+const cities = require(`./cities`);
+const { places, descriptors } = require(`./seedHelpers`);
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelp-camp", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`MONGO CONNECTION OPEN!!`);
+  })
+  .catch((err) => {
+    console.log(`ERROR MONGO CONNECTION`);
+    console.log(err);
+  });
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const rand1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://source.unsplash.com/collection/483251`,
+      description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti vitae corporis voluptatibus harum nostrum? Esse sint voluptatibus dolorum, quis repellendus rem impedit facere ad, expedita qui animi perferendis illo id.`,
+      price,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
