d6d8e87a87c431de7af248100db74aa2f6185a36
Anirudhwa Ankon <ankon495@gmail.com>
2023-02-13 20:34:16 +0600

    fixed port number

--- app.js
+++ app.js
@@ -150,6 +150,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render(`error`, { err });
 });
 const port = process.env.PORT || 3000;
-app.listen([port], () => {
+app.listen(port, () => {
   console.log(`On port ${port}`);
 });
